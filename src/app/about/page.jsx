97a13e9ddbb006544a1b2da58b87f43e6a8566fc
gr8fulnez <oluwolejohn72@gmail.com>
2024-10-23 12:44:48 -0700

    about page setup

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from "./about.module.css";
+
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div className={styles.imgContainer}>
+
+    {/* <Image src="/about.png" alt='' fill /> */}
+      <Image src="https://images.pexels.com/photos/28871575/pexels-photo-28871575/free-photo-of-scenic-view-of-lush-green-landscape-with-trees.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill />
+    </div>
   )
 }
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,29 +1,34 @@
+"use client"
+
+import { useState } from 'react'
 import styles from './links.module.css'
 import NavLink from "./navLink/navLink"
 
+ const links = [
+    {
+        title: "Homepage",
+        path: "/"
+    },
+    {
+        title: "About",
+        path: "/about"
+    }, {
+        title: "Contact",
+        path: "/contact"
+    }, {
+        title: "Blog",
+        path: "/blog"
+    },
+]
 const Links = () => {
-
-    const links = [
-        {
-            title: "Homepage",
-            path: "/"
-        },
-        {
-            title: "About",
-            path: "/about"
-        }, {
-            title: "Contact",
-            path: "/contact"
-        }, {
-            title: "Blog",
-            path: "/blog"
-        },
-    ]
+const [open, setOpen] = useState(false)
+   
 
     const session = true 
     const isAdmin = true 
 
     return (
+        <div className={styles.container}>
         <div className={styles.links}>
             {
                 links.map((link => (
@@ -41,7 +46,15 @@ const Links = () => {
                     <NavLink item={{ title: "Login", path: "/login" }} />
                 )
             }
-
+            </div >
+            <button className={styles.menuButton} onClick={()=> setOpen((prev) => !prev)}>Menu</button>
+            {open && (
+                <div className={styles.mobileLinks}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.title} />
+                    ))}
+                </div>
+            )}
         </div >
     )
 }
