584b8f04482c8af206c3ffbb6811757bc02c4d35
gr8fulnez <oluwolejohn72@gmail.com>
2024-10-24 01:45:47 -0700

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,53 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
 import styles from "./about.module.css";
 
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
 
-export default function AboutPage() {
+
+const AboutPage = () => {
+
+  // console.log("lets check where it works")
   return (
-    <div className={styles.imgContainer}>
-
-    {/* <Image src="/about.png" alt='' fill /> */}
-      <Image src="https://images.pexels.com/photos/28871575/pexels-photo-28871575/free-photo-of-scenic-view-of-lush-green-landscape-with-trees.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt='' fill />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={styles.img}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default AboutPage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>Lama creative thoughts agency @ All rights reserved.</div>
+    </div>
   )
 }
